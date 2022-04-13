ae1f70f3fbe1fb75cee62168c9fcc07c3909548d
conoroheocha <conoroheocha@gmail.com>
2022-04-13 17:40:38 +0100

    adoRead

---
+++ adoRead.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, between, task
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(0, 15)
+
+    def on_start(self):
+        self.getTimes()
+
+    @task
+    def getTimes(self):
+        self.client.get(
+            "/read",
+            json={
+                "ADO_email": "charles92@example.net",
+                "Athlete_email": "qroach@example.com",
+            },
+        )
+
+    # @task
+    # def about(self):
+    #     self.client.get("/about/")
